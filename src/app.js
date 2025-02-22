1c84a2584b490f7a996d2915bdaf31e732e9b7da
joposada7 <joposada7@gmail.com>
2025-02-21 20:44:36 -0600

    remove unnecessary requires

--- src/app.js
+++ src/app.js
@@ -1,13 +1,5 @@
 const express = require('express');
-const http = require('http');
-const https = require('https');
 const path = require('path');
-const fs = require('fs');
-
-const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/posadaj.com/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/posadaj.com/fullchain.pem')
-};
 
 const app = express();
 app.get('/', (req, res) => {
