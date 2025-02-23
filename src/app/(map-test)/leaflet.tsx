e7ce9cb46fbd8021bc3c43a48a520757c697817a
Roberto Perez <robertoperezl763@gmail.com>
2025-02-22 22:34:35 -0600

    checking if default map runs in prod env

--- src/app/(map-test)/leaflet.tsx
+++ src/app/(map-test)/leaflet.tsx
@@ -12,11 +12,15 @@ const Leaflet = () => {
         iconUrl: "../markers.png",
         iconSize: [38,38]
     })
+
+    const mapURL = 1 ===1 ? "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" :
+    "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png";
+
     return (
         <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
             <TileLayer
                 attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url='https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png' 
+                url= {mapURL} 
             />
             <Marker position={[51.505, -0.09]} icon={customIcon}>
                 <Popup>
